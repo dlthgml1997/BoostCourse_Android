de67209a9973daca5bbdd0fa1299b73d602bf968
leesohee <shfksthgml@naver.com>
2019-08-05 18:01:32 +0900

    layout 생성

---
+++ BoostCourse_PJT/app/src/main/java/com/sohee/boostcourse_pjt/ReviewDetailActivity.java
@@ -0,0 +1,13 @@
+package com.sohee.boostcourse_pjt;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReviewDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_detail);
+    }
+}
---
+++ BoostCourse_PJT/app/src/main/java/com/sohee/boostcourse_pjt/WriteReviewActivity.java
@@ -0,0 +1,13 @@
+package com.sohee.boostcourse_pjt;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WriteReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_review);
+    }
+}
